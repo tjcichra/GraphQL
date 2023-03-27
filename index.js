cef3608d937957eb44eab866f2f1d5b59e135cf4
tjcichra <8120075+tjcichra@users.noreply.github.com>
2023-03-26 22:40:25 -0400

    add

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 let { graphql, buildSchema } = require('graphql');
 
 let schema = buildSchema(`
+  type User {
+    firstname: String
+    lastname: String
+  }
+
   type Query {
     id: ID
-    hello: String
+    user(id: ID): User
   }
 `);
 
-let rootValue = { id: () => 1, hello: () => 'Hello world!' };
+let rootValue = {
+  id: 1,
+  user: (args) => {
+    return {
+      firstname: 'terence' + args.id,
+      lastname: 'fletcher'
+    }
+  }
+};
 
-let source = '{ id, hello }';
+let source = '{ id, user(id: 3) { firstname, lastname } }';
 
 graphql({ schema, source, rootValue }).then((response) => {
   console.log(response);
