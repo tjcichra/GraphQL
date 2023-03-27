a800dcfafc5ed1a031051feebd272d29c2450c66
tjcichra <8120075+tjcichra@users.noreply.github.com>
2023-03-26 21:20:20 -0400

    added id

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import { graphql, buildSchema } from 'graphql';
+let { graphql, buildSchema } = require('graphql');
 
 let schema = buildSchema(`
   type Query {
+    id: ID
     hello: String
   }
 `);
 
-let rootValue = { hello: () => 'Hello world!' };
+let rootValue = { id: () => 1, hello: () => 'Hello world!' };
 
-let source = '{ hello }';
+let source = '{ id, hello }';
 
 graphql({ schema, source, rootValue }).then((response) => {
   console.log(response);
