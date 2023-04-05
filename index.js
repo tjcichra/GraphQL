cce76141a01d01d518580f90faba09ddba9bf918
tjcichra <8120075+tjcichra@users.noreply.github.com>
2023-04-04 23:32:35 -0400

    i'll be there for you

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 let { graphql, buildSchema } = require('graphql');
 
 let schema = buildSchema(`
-  type User {
-    firstname: String
-    lastname: String
+  type Character {
+    firstname: String!
+    lastname: String!
+    catchphrase: String
   }
 
   type Query {
-    id: ID
-    user(id: ID): User
+    character(id: ID!): Character
   }
 `);
 
 let rootValue = {
-  id: 1,
-  user: (args) => {
-    return {
-      firstname: 'terence' + args.id,
-      lastname: 'fletcher'
+  character: (args) => {
+    switch (args.id) {
+      case '1':
+        return {
+          firstname: 'Ross',
+          lastname: 'Geller',
+          catchphrase: 'We were on a break!'
+        }
+      case '2':
+        return {
+          firstname: 'Chandler',
+          lastname: 'Bing',
+          catchphrase: 'Could I BE anymore...'
+        }
+      case '3':
+        return {
+          firstname: 'Joey',
+          lastname: 'Tribbiani',
+          catchphrase: 'How YOU doin?'
+        }
+      default:
+        break;
     }
   }
 };
 
-let source = '{ id, user(id: 3) { firstname, lastname } }';
+let source = `query characters($id: ID!) {
+  character(id: $id) {
+    firstname,
+    lastname,
+    catchphrase
+  }
+}`;
 
-graphql({ schema, source, rootValue }).then((response) => {
+let variableValues = {
+  id: 3
+};
+
+graphql({ schema, source, rootValue, variableValues }).then((response) => {
   console.log(response);
 });
